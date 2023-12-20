a7d1f106f66990d60ffe73b1f7d101cd28c0113f
Kagwe Muchane <kamuchan@ncsu.edu>
2023-12-20 15:29:00 -0500

    update

--- client/src/utils/urls.tsx
+++ client/src/utils/urls.tsx
@@ -1,7 +1,7 @@
 export const serverUrl = (process.env.NODE_ENV === 'development') 
 ? 'http://localhost:8080'
-: '';
+: 'https://ncsu-cure-72d273a2f021.herokuapp.com/';
 
 export const hostUrl = (process.env.NODE_ENV === 'production') 
-? ""
+? "https://ncsu-cure-72d273a2f021.herokuapp.com/"
 : "http://localhost:3000";
--- server/src/utils/urls.ts
+++ server/src/utils/urls.ts
@@ -1,14 +1,14 @@
 export const secureServerUrl = (process.env.NODE_ENV === 'production') 
-? 'https://domain.com'
+? 'https://ncsu-cure-72d273a2f021.herokuapp.com/'
 :  'https://localhost:9090';
 
 export const serverUrl = (process.env.NODE_ENV === 'production') 
-? 'https://domain.com'
+? 'https://ncsu-cure-72d273a2f021.herokuapp.com/'
 :  'http://localhost:8080';
 
 
 export const hostUrl = (process.env.NODE_ENV === 'production') 
-? "https://www.domain.com"
+? "https://ncsu-cure-72d273a2f021.herokuapp.com/"
 : "http://localhost:3000";
 
 
